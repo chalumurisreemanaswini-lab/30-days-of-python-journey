d40c72fbdc581fd9c148632e09f172015b4f4422
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-08 19:39:40 +0000

    Day 2: Mastered primary data types (Part 1)
    Successfully finished working on the first part of Day 2. I learned and developed a deep understanding of the data boxes. I even figured out how to fix a bug of my first dictionary! It was far better today. On to the next day.

---
+++ day_02_data_types.py
@@ -0,0 +1,24 @@
+# ---DAY TWO: THE DATA BOXES---
+
+# 1. String (Str) - For text
+subject = "Computer Science"
+
+# 2. Integer (int) - For whole numbers
+days_completed = 2
+
+# 3. Boolean (bool) - Great for yes/no questions
+is_student = True
+has_finished_day_1 = True
+
+# 4. Dictionary (dict) - Great for a "Student ID card"
+# it pairs a label (Key) with information (Value) 
+my_profile = {
+  "name": "Your Name",
+  "goal": "STEM SCHOLAR",
+  "status": "Active"
+}
+
+# ---REFLECTION---
+# I decided to split day 2 into two parts to make sure I have a deep understanding of each data type.
+# Today I focused on basic data types and dictionaries. 
+# Tomorrow I will continue with this and this pace will ensure I manage my learning effectively.

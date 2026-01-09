d75275c03212c4b03df605f458af264629faaeeb
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-09 21:34:26 +0000

    Day 3: Exploring Collections and practical use-cases for Sets
    I completed my study of data types by learning about Lists, Tuples, and Sets. I researched how Sets could be used. They can be used for things like Attendance Trackers so even if you type a name twice, it'll only count as one. I am proud of what I have accomplished. So far so good!

---
+++ day_03_data_typesPt2/day_3.py
@@ -0,0 +1,33 @@
+# ---DAY TWO: THE DATA BOXES---
+# (continued)
+
+# 1. List (list) - Lists are ordered and you can change them. They use square brackets []
+# A list of my favourite food
+Food = ["Pasta", "tacos", "wraps"]
+
+# 2. Tuple - Tuples are like lists, but you cannot change them once they are made. They use parentheses
+# Constants that shouldn't change, like a birthday
+birthday = (1, "January", 2014)
+
+# 3. Set (set) -  Sets have no order and no duplicates. They use curly brackets {}
+unique_topics = {"Data Types", "Variables", "Python", "Python"}
+
+# The difference between a List and a Set
+example_list = ["apple", "apple", "cherry"]
+example_set = {"apple", "apple", "cherry"}
+
+print("List allows duplicates:", example_list)
+print("Set removes duplicates:", example_set)
+
+#---REFLECTION---
+
+# I learned more data boxes adding on to the previous data boxes I have learned yesterday. 
+# My progress really lets me appreciate how much just little steps make a big difference.
+# I realised it all about consistency and moving at your own pace.
+# I remember on day 1, how it took so long to understand and work the controls.
+# A little research I did, I was wondering what sets could prove useful for.
+#Attendance Tracker: Maintain a set of students who attended a class. Use .add() to record names;
+# since it’s a set, students won't be counted twice even if you add them again.
+# So that is a cool way to use the tool!
+
+ 

12457b26d558fd81e3de8ed64d751c43f0592e7e
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-10 20:19:03 +0000

    Day 4: Variables
    I added a Python script demonstrating variable declaration, user input, basic arithmetic operations, and area calculation of a circle. I am getting the hang of variables and I completed a practical with it. So far so good!

---
+++ day_04_variables/day_4.py
@@ -0,0 +1,48 @@
+# ---DAY FOUR: VARIABLES---
+
+# 1. Declaring variables with snake_case naming
+first_name = "YourName"
+last_name = "YourLastName
+age = 12
+is_student = True
+
+# 2. Asking for user information
+user_name = input("Enter your name: ")
+birth_year = input("What year were you born? ")
+
+# 3. Practice "Casting" the string to an integer for math
+age = 2026 - int(birth_year)
+print(f"Hello{username} you are {age} years old!")
+
+# Basic Arithmetic with Variables
+num_one = 6
+num_two = 3
+
+sum_result = num_one + num_two
+diff = num_one + num_ two
+product = num_two * num_two
+remainder = num_one % num_one
+quotient = num_one / num_two
+floor_result = num_one // num_two
+power_result = num_one ** num_two
+
+# AREA OF A CIRCLE
+#  Step 1: the radius
+radius = 30
+
+# Step 2: Write the formula
+# We use () to make sure squaring happens before the multiplication
+area_of_circle = 3.14 * (radius ** 2)
+
+# Step 3: print the answer
+print("The area of the circle is:", area_of_circle)
+
+# ---REFLECTION---
+
+# Today I worked on Variables moving on from Data Types.
+# I made sure to take multiple tutorials to deepen my understanding.
+# Though, according to my curriculum, I should be on Strings.
+# But I knew the more construstive approach was to take my time
+# and really be able to confidentally code with the topic.
+# I also learned how to calculate the area of a circle by using the exponential operator (**)."
+# I am glad I came this far, so I will keep it up!

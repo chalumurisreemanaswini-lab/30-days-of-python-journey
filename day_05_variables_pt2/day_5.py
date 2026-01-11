6f4c4e5b7e428dcba3307609eb6188e4e1352c95
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-11 21:13:20 +0000

    Day 5: Variables and combining two strings
    I decided to continue the topic of Variables to make sure I gain the expertise. I am glad I came this far! So far so good!

---
+++ day_05_variables_pt2/day_5.py
@@ -0,0 +1,41 @@
+# ---DAY FIVE: VARIABLES---
+# (continued)
+
+# 1. Combining two strings:
+# Create two separate "boxes"
+first_name = "Jane"
+last_name = "Doe"
+
+# We combine them into a third "box"
+# We add in the " " in the middle so there is a space between the names!
+full_name = first_name + " " + last_name
+
+print(full_name) # This will now show: Jane Doe
+
+# 2. Updating a Variable 
+# Start with 0
+my_score = 0
+
+# Now, tell the Variable to be "whatever it was before, plus 10"
+my_score = my_score + 10
+
+print(my_score) # This will now show: 10
+
+# 3. Changing a number into Text
+
+age = 12
+
+# This tells Python: Turn the number 12 into the word '12'"
+age_as_text = str(age)
+
+print("I am " + age_as_text + "years old.")
+
+# ---REFLECTION---
+
+#  Today I learned how to combine two strings.
+# I also proudly fixed my own code without any help!
+# I also figured out through a step by step process on how to change a number into Text.
+# Although, I couldn't exactly grasp the code so I used the help of other resources and tutorials!
+# I got through the learning obstacles and i'm now confident with changing number into text in Python.
+# This is day 5 and I am still keeping strong and focused! :)
+
